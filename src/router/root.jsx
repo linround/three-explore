2f81091e124b3fd8c80627ad5adc1d349678b508
linyuan <673609957@qq.com>
2023-07-21 15:21:56 +0800

    refactor: shader fragment complex

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom'
 import css from './root.module.less'
+import { ReactComponent as  Github } from '../assets/github.svg'
 
 export default function Root() {
+  const href = 'https://github.com/linround/three-explore'
   return (
     <>
       <div className={css.head}>
-        lin round
+        <a href={href} target='_blank' rel="noreferrer">
+          lin round
+        </a>
       </div>
       <Outlet />
     </>
