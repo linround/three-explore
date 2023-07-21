d30ab1454c892de49c61e79f440ff28b2c7032c1
linyuan <673609957@qq.com>
2023-07-21 21:02:02 +0800

    chore: delete text

--- src/view/geometry/colorVertexShader.glsl
+++ src/view/geometry/colorVertexShader.glsl
@@ -5,5 +5,6 @@ void main() {
     // 在这里将该顶点的uv坐标传递到全局变量vUv,在块着色器中接受该顶点的uv坐标
     vUv = uv;
     // 投影矩阵*模型视图矩阵*顶点坐标
+    // projectionMatrix和modelViewMatrix 这两个矩阵是由threejs 提供
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
