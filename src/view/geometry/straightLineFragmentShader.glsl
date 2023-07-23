0e74a8cced4c78ab373a98cbc3b55db99409c2dd
linyuan <673609957@qq.com>
2023-07-23 15:59:18 +0800

    feat: draw line

--- src/view/geometry/straightLineFragmentShader.glsl
+++ src/view/geometry/straightLineFragmentShader.glsl
@@ -6,8 +6,13 @@ uniform vec2 iMouse;
 
 // 这里 x y 差值范围在[-0.02 , 0.02] 之间的值，会返回一个平滑值
 // 相差在 范围之外的 返回的是1这个值
+
+
+// 这里传入的是一个绝对值
+// 所以xy绝对值在范围内时，取得范围内的一个 差值结果；
+// 在范围外时（也就是一定大于上边界），取得的结果时 1；
 float plot(vec2 st){
-    return smoothstep(0.0,0.02,abs(st.y-st.x));
+    return smoothstep(0.0,0.01,abs(st.y-st.x));
 }
 
 void main() {
