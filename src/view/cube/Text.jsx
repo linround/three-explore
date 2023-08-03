4acb1230bb8302e02d42cbd4401d470ef98f6bb9
linyuan <673609957@qq.com>
2023-08-03 19:38:25 +0800

    chore: add quaternion url

--- src/view/cube/Text.jsx
+++ src/view/cube/Text.jsx
@@ -4,8 +4,12 @@ export function Text() {
       <h1>立方体的2D投影</h1>
       <p>实现将立方体投影线在2D平面上，并进行显示</p>
       <ul>
-        <li><a target={'_blank'} rel="noreferrer" href={'https://www.bilibili.com/video/BV1SW411y7W1/?vd_source=2fbc276c906dcfb63eeb8b5cf37bd9ff'}>四元数的可视化；</a></li>
-        <li><a target={'_blank'} rel="noreferrer" href={'https://krasjet.github.io/quaternion/quaternion.pdf'} >四元数与三维旋转；</a></li>
+        <li>四元数的可视化；<a target={'_blank'} rel="noreferrer" href={'https://www.bilibili.com/video/BV1SW411y7W1/?vd_source=2fbc276c906dcfb63eeb8b5cf37bd9ff'}>（视频）</a></li>
+        <li>四元数与三维旋转；<a target={'_blank'} rel="noreferrer" href={'https://krasjet.github.io/quaternion/quaternion.pdf'} >（pdf）11/73</a></li>
+        <ul>
+          <li>复数相乘可以看作：一个转换矩阵乘以另一个复数向量；</li>
+          <li>复数相乘可以看作：旋转与缩放的复合；</li>
+        </ul>
         <li>构建立方体</li>
         <></>
       </ul>
