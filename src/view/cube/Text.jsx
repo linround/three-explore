bb3966b530ee39f0ace7671225070b6faccff415
linyuan <673609957@qq.com>
2023-08-08 09:06:37 +0800

    chore:aspect ratio

--- src/view/cube/Text.jsx
+++ src/view/cube/Text.jsx
@@ -11,6 +11,7 @@ export function Text() {
       <h1>立方体的2D投影</h1>
       <p>实现将立方体投影线在2D平面上，并进行显示</p>
       <ul>
+        <li><a href={'https://www.shadertoy.com/view/4dG3RK'}>立方体示例1</a></li>
         <li>四元数的可视化；<a target={'_blank'} rel="noreferrer" href={'https://www.bilibili.com/video/BV1SW411y7W1/?vd_source=2fbc276c906dcfb63eeb8b5cf37bd9ff'}>（视频）</a></li>
         <li>四元数与三维旋转；<a target={'_blank'} rel="noreferrer" href={'https://krasjet.github.io/quaternion/quaternion.pdf'} >（pdf）29/73</a></li>
 
