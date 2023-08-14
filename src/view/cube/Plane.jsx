32fbe6b6660ce6c5af8beaace70bf7083420eede
linyuan <673609957@qq.com>
2023-08-14 18:35:08 +0800

    chore:add shaderToy link

--- src/view/cube/Plane.jsx
+++ src/view/cube/Plane.jsx
@@ -3,6 +3,10 @@ export function Plane() {
     <>
       <main>
         <h1>立方体面的2D投影</h1>
+        <ul>
+          <li><a href={'https://www.shadertoy.com/view/mdsfR7'}>2D平面的 fov角，裁切</a></li>
+          <li><a href={'https://www.shadertoy.com/view/DsXfWn'}>2D平面的 正交投影，裁切</a> </li>
+        </ul>
       </main>
     </>
   )
