b2999899c1e0ae427c606a9efe17c902af50d51a
linyuan <673609957@qq.com>
2023-08-22 20:15:17 +0800

    chore:curve

--- src/view/light/Text.jsx
+++ src/view/light/Text.jsx
@@ -5,6 +5,9 @@ export function Text() {
         <h1>光照在模型中的实现</h1>
         <p>附录：</p>
         <ul>
+          <li><a href={'https://yilingui.xyz/attachments/spatial_ds--bsp_tree-octree-kd-tree.pdf'}>
+            四叉树、八叉树、bsp树、kd树
+          </a></li>
           <li><a href={'https://kirisamer.github.io/2022/11/05/Ch4-COMP37111/'}>渲染方程介绍；</a></li>
           <li><a href={'https://zhuanlan.zhihu.com/p/636749237'}>Lambert模型；</a></li>
           <li><a href={'https://www.liaomz.top/2022/03/12/tu-xing-xue-ji-chu-chuan-tong-de-jing-yan-guang-zhao-mo-xing/'}>传统光照模型；</a> </li>
