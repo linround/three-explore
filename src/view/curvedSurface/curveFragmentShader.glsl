459b858e6fa1ca1641d359c526892d9c40493091
linyuan <673609957@qq.com>
2023-08-22 13:40:12 +0800

    chore:curve 相关

---
+++ src/view/curvedSurface/curveFragmentShader.glsl
@@ -0,0 +1,18 @@
+uniform vec3 iResolution;
+uniform vec2 iMouse;
+uniform float iTime;
+uniform sampler2D iChannel0;
+#define PI 3.1415926
+#define TWO_PI 6.28318530718
+
+
+void renderCurve(in vec2 st){
+    vec3 color = vec3(st,0.);
+    gl_FragColor = vec4(color,1.0);
+}
+
+
+void main() {
+    vec2 st = gl_FragCoord.xy/iResolution.xy;
+    renderCurve(st);
+}
--- src/view/curvedSurface/curvedSurface.jsx
+++ src/view/curvedSurface/curvedSurface.jsx
@@ -2,11 +2,76 @@ import { Component, createRef } from 'react'
 import css from './css.module.less'
 import { CanvasComponent } from '../../component/canvas.jsx'
 import { Text } from './Text.jsx'
+import * as THREE from 'three'
+import bayer from '../../assets/bayer.png'
+import { resizeRendererToDisplaySize } from '../../utils.js'
+
+import commonVertexShader from '../../common/commonVertexShader.glsl?raw'
+import curveFragmentShader from './curveFragmentShader.glsl?raw'
 
 export class CurvedSurface extends Component {
   constructor(props) {
     super(props)
     this.canvas = createRef()
+    this.renderScene = this.renderScene.bind(this)
+    this.renderCurve = this.renderCurve.bind(this)
+  }
+
+  componentDidMount() {
+    this.renderCurve()
+  }
+
+  renderCurve() {
+    this.renderScene(curveFragmentShader, commonVertexShader)
+  }
+  renderScene(fragmentShader, vertexShader) {
+    const canvas = this.canvas.current
+    const renderer = new THREE.WebGLRenderer({ canvas, })
+    const camera = new THREE.OrthographicCamera(
+      -1, 1, 1, -1, -1, 1
+    )
+    const scene = new THREE.Scene()
+    const plane = new THREE.PlaneGeometry(2, 2)
+    const loader = new THREE.TextureLoader()
+    const texture = loader.load(bayer)
+    texture.minFilter = THREE.NearestFilter
+    texture.magFilter = THREE.NearestFilter
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+    const uniforms = {
+      iTime: { value: 0, },
+      iResolution: { value: new THREE.Vector3(), },
+      iMouse: { value: new THREE.Vector2(), },
+      iChannel0: { value: texture, },
+    }
+    const material = new THREE.ShaderMaterial({
+      fragmentShader,
+      vertexShader,
+      uniforms,
+    })
+
+    scene.add(new THREE.Mesh(plane, material))
+    let x = 0
+    let y = 0
+
+    const render = (t) => {
+      const time = 0.001 * t
+      resizeRendererToDisplaySize(renderer)
+      uniforms.iResolution.value.set(
+        canvas.width, canvas.height, 1
+      )
+      uniforms.iMouse.value.set(x, y)
+      uniforms.iTime.value = time
+
+      renderer.render(scene, camera)
+      requestAnimationFrame(render)
+
+    }
+    requestAnimationFrame(render)
+    canvas.addEventListener('pointermove', (e) => {
+      x = e.offsetX
+      y = canvas.height - e.offsetY
+    })
   }
   render() {
     return (
