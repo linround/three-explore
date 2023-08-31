d472443c6174ff1e973c7f5675d89a8e79df3fc0
linyuan <673609957@qq.com>
2023-08-31 13:33:05 +0800

    feat:sdf

--- src/view/curvedSurface/Text.jsx
+++ src/view/curvedSurface/Text.jsx
@@ -2,7 +2,8 @@ export function Text() {
   return (
     <main>
       <h1>关于如何构建曲面</h1>
-      <li><a href={'https://zhuanlan.zhihu.com/p/586364840'}>投影矩阵</a></li>
+      <p>附录：</p>
+      <li><a href={'https://zhuanlan.zhihu.com/p/586364840'}>彻底理解投影矩阵</a></li>
       <p>待完成</p>
     </main>
   )
