9ecf8b4828e16f7605aaea10026a5bb8234762ca
linyuan <673609957@qq.com>
2023-08-31 09:21:18 +0800

    feat:sdf

--- src/view/sdf/Text.jsx
+++ src/view/sdf/Text.jsx
@@ -7,6 +7,9 @@ export function Text() {
         <li><a href={'https://www.zhihu.com/column/c_1431297491883384833'}>符号距离场函数（SDF）</a></li>
         <li><a href={'https://iquilezles.org/articles/distfunctions2d/'}>2D distance functions</a></li>
       </ul>
+      <iframe width="640" height="360" frameBorder="0"
+        src="https://www.shadertoy.com/embed/mlfBWB?gui=true&t=10&paused=true&muted=false"
+        allowFullScreen></iframe>
       <li>SDF简介</li>
       <p>SDF就是某一点到某一个区域边界的最短距离，求SDF的通用思路就是分类讨论；</p>
     </main>
