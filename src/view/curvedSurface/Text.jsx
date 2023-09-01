db75ad839da6c682affe51cbe8d573e9859c4fd1
linyuan1105 <673609957@qq.com>
2023-09-01 19:16:00 +0800

    fix:z buffer test

--- src/view/curvedSurface/Text.jsx
+++ src/view/curvedSurface/Text.jsx
@@ -4,6 +4,8 @@ export function Text() {
       <h1>关于如何构建曲面</h1>
       <p>附录：</p>
       <li><a href={'https://zhuanlan.zhihu.com/p/586364840'}>彻底理解投影矩阵</a></li>
+      <li><a href={'https://feiqi3.cn/blog/74'}>Ray Marching和SDF</a></li>
+      <li><a href={'https://en.wikipedia.org/wiki/Directional_derivative'}>Directional_derivative</a></li>
       <p>待完成</p>
     </main>
   )
