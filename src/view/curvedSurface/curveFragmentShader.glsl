2028be4cb33805b83c04cfa20660c3476c9ed326
linyuan <673609957@qq.com>
2023-09-01 12:29:48 +0800

    feat:sdf

--- src/view/curvedSurface/curveFragmentShader.glsl
+++ src/view/curvedSurface/curveFragmentShader.glsl
@@ -8,7 +8,6 @@ uniform sampler2D iChannel0;
 
 // epsilon-type values
 const float S = 0.01;
-const float EPSILON = 0.01;
 
 // const delta vectors for normal calculation
 const vec3 deltax = vec3(S ,0, 0);
@@ -17,7 +16,7 @@ const vec3 deltaz = vec3(0 ,0, S);
 
 // 这里有些类似3位的SDF
 float distanceToNearestSurface(vec3 p){
-    float s = 1.0;// 定义了长宽高的值
+    float s = 2.0;// 定义了长宽高的值
     vec3 d = abs(p) - vec3(s);
     return min(max(d.x, max(d.y,d.z)), 0.0) //这是计算内部的点 到表面的最小距离（负值）
     + length(max(d,0.0));// 这是计算外部坐标点 到表面的距离（正值）
@@ -43,8 +42,8 @@ vec3 computeLambert(vec3 p, vec3 n, vec3 l){
 // p 相机位置
 // dir 相机位置指向观察平面上某一点的位置的向量
 
-// p 是固定的
-// dir 是随着 x,y变化而变化
+// p 是固定的 cameraPosition
+// dir 是随着 x,y变化而变化，实际是由点cameraPosition 指向 由  nright和 cameraUp 组成的平面上的点
 
 // 所以当cameraDirection 方向越大，x,y 所能扩展的范围也越小
 // cameraDirection 越小，x,y 所能扩展的范围也越小
@@ -52,14 +51,31 @@ vec3 intersectWithWorld(vec3 p, vec3 dir){
     float dist = 0.0;
     float nearest = 0.0;
     vec3 result = vec3(0.0);
-    // 计算
-    for(int i = 0; i < 8; i++){
+    // 计算cameraPosition 出发，沿着dir，移动dist个单位
+    // 第一次循环时，
+    // i的 的次数多少 与立方体的大小有关
+    // i 8   r=1
+    // i 13  r=2
+    for(int i = 0; i < 13; i++){
         // 计算到表面的点的最近的距离
-        nearest = distanceToNearestSurface(p + dir*dist);
+        // i= 0时，p+0 此时计算的是 cameraPosition 到立方体表面的最小距离 即沿着dir方向的距离 d0;此时d0范围较大
+        // d0 值太大，此时没有计算出该点 直接在 进入i=1;
+        // i= 1时，p+dir*d0 此时计算的是点实际就是立方体表面的点，计算的距离 d1=0
+        // 通过将点沿着dir方向移动了d0距离，此时该点进入 d1的值在范围内，说明当前点属于立方体表面，即在改方向上找到了 对应的坐标点
+        // 计算该店在模型中的位置，得到对应的颜色，直接跳出即可
+
+        // 如果此时还未能在该方向上找到对应点，继续沿着斜线方向移动，直到找到最近的点
+
+        vec3 point = p+dir*dist;
+        // 此处根据 p点的位置和方向 设置光源的位置
+        vec3 light = p-4.*dir;
+
+        nearest = distanceToNearestSurface(point);
+
+        // 如果点 p+dir*dist 距离表面在某个范围EPSILON 内，则被认为是表面的点
+        float EPSILON = 0.01;
         if(nearest < EPSILON){
-            vec3 hit = p+dir*dist;
-            vec3 light = vec3(100.0, 30.0, 50.0);
-            result = computeLambert(hit, computeSurfaceNormal(hit), light);
+            result = computeLambert(point, computeSurfaceNormal(point), light);
             break;
         }
         dist += nearest;
