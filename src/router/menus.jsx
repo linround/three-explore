0f13d1b247fae26b7d401771e2bc5f3a0fb1ce5e
linyuan <673609957@qq.com>
2023-09-11 16:18:43 +0800

    feat:action

--- src/router/menus.jsx
+++ src/router/menus.jsx
@@ -4,7 +4,7 @@ export default function Menus() {
   return (
     <div>
       <ul>
-        <li><Link to='shader'>在 threejs 的 shader 中的常见变量；</Link></li>
+        <li><Link to='shader'>在 threejs 的 shader 中的常见变量；tag</Link></li>
         <li><Link to='geometry'>shader 几何生成；</Link></li>
         <li><Link to='color'>color 的奇妙 idea ；</Link></li>
         <li><Link to='variety'>图案的变化：平移、旋转、缩放；</Link></li>
