953dc51394d4fad15a34ad7910322bbfc120b9c6
linyuan <673609957@qq.com>
2023-10-19 18:03:31 +0800

    feat: renderer

---
+++ src/view/renderer/layer/ObservationPoint.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export class ObservationPoint extends React.Component {
+
+}
--- src/view/renderer/layer/layer.jsx
+++ src/view/renderer/layer/layer.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import css from './css.module.less'
 import { LayerWrapper } from './layerWrapper.js'
+import items from './items.js'
 export class Layer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChangeObservationPoint = this.handleChangeObservationPoint.bind(this)
+    this.state = { ...items, }
+  }
+  handleChangeObservationPoint() {
+
+  }
 
   render() {
     return (
       <LayerWrapper>
         <div className={css.container}>
-          layer
+
         </div>
       </LayerWrapper>
     )
