a74daec95f26e9775d79b172733cc55bd2b1c45a
linyuan <673609957@qq.com>
2023-11-02 18:08:48 +0800

    feat: renderer

--- src/view/renderer/layer/layer.jsx
+++ src/view/renderer/layer/layer.jsx
@@ -27,6 +27,17 @@ export class Layer extends React.Component {
         <div className={css.container} ref={this.layerContainer}>
           <ObservationPoint />
           <ObservationDirection />
+          <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+
+            <path d="M10 10"/>
+            <path d="M 0 0 C 100 0, 100 200, 200 200" stroke="red" fill="blue" strokeWidth="6px"/>
+
+            {/*Points*/}
+            <circle cx="100" cy="0" r="10" fill="red"/>
+            <circle cx="200" cy="0" r="10" fill="red"/>
+            <circle cx="200" cy="0" r="10" fill="red"/>
+
+          </svg>
         </div>
       </LayerWrapper>
     )
--- src/view/renderer/layer/paint.js
+++ src/view/renderer/layer/paint.js
@@ -3,6 +3,40 @@ export function paintSvg(container) {
   if (!container) return
   const observationPointOutPutNode =  container.querySelector('#' + sceneData.observationPoint.outPutNode)
   const observationDirectionInPutNode =  container.querySelector('#' + sceneData.observationDirection.inPutNode)
-  console.log(observationPointOutPutNode)
-  console.log(observationDirectionInPutNode)
+  createConnection(
+    observationDirectionInPutNode,
+    observationPointOutPutNode,
+    container
+  )
+}
+export function createConnection(
+  input, output, container
+) {
+  const inputRect = input.getBoundingClientRect()
+  const outputRect = output.getBoundingClientRect()
+  const containerRect = container.getBoundingClientRect()
+  console.log(inputRect)
+  console.log(outputRect)
+  console.log(containerRect)
+}
+export function createSvg(ele) {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+  svg.appendChild(ele)
+  return svg
+}
+export function createPath(d) {
+  const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+  path.setAttributeNS(
+    null, 'd', d
+  )
+  return path
+}
+export function createCurve(
+  startX, startY, endX, endY
+) {
+  const x1 = (startX + endX) / 2.
+  const y1 = startY
+  const x2 = (startX + endX) / 2.
+  const y2 = endY
+  return `M ${startX} ${startY} C ${x1} ${y1} ${x2} ${y2} ${endX} ${endY}`
 }
