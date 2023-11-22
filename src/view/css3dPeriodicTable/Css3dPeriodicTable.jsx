6d92ccb2dfe477e5b8e736a665f91a2fbbcb337c
linyuan <673609957@qq.com>
2023-11-22 18:02:27 +0800

    feat:

---
+++ src/view/css3dPeriodicTable/Css3dPeriodicTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class Css3dPeriodicTable extends React.Component {
+  render() {
+    return (
+      <>
+        https://threejs.org/examples/#css3d_periodictable
+      </>
+    )
+  }
+}
