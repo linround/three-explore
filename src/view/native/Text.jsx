18231374d1c05c57e2a9646a56f2c0ecdbf88d9d
linyuan <yuanlincuc@gmail.com>
2023-12-04 14:32:09 +0800

    feat: draw line-nurbs-surface

--- src/view/native/Text.jsx
+++ src/view/native/Text.jsx
@@ -2,7 +2,9 @@ export function Text() {
   return (
     <main>
       <h1>使用 threejs 定义顶点、法向量；使用 webgl 自定义顶点、法向量；</h1>
-      <p>待完成</p>
+      <h2>threejs</h2>
+      <h3>setAttribute</h3>
+      可设置顶点的position、color的索引；
     </main>
   )
 }
