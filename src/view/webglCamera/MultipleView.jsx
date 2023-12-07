249afc52f2120081ded843b7ad4f1e513f5ddba8
linyuan <yuanlincuc@gmail.com>
2023-12-07 16:36:33 +0800

    feat: camera

--- src/view/webglCamera/MultipleView.jsx
+++ src/view/webglCamera/MultipleView.jsx
@@ -2,6 +2,7 @@ import { MultipleViewText } from './MultipleViewText.jsx'
 import React, { createRef } from 'react'
 import css from './css.module.less'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 export class MultipleView extends React.Component {
   constructor(prop) {
     super(prop)
@@ -16,25 +17,120 @@ export class MultipleView extends React.Component {
     this.renderScene()
   }
   renderScene() {
+    const canvas = this.canvas.current
     const { canvasWidth, canvasHeight, } = this.state
     const aspect = canvasWidth / canvasHeight
     const scene = new THREE.Scene()
+
+    // 一个透视相机
     const camera = new THREE.PerspectiveCamera(
+      10,
+      0.5 * aspect,
+      1,
+      1000
+    )
+    // 设置相机位置
+    camera.position.z = 100
+
+    // 一个透视相机
+    const cameraPerspective = new THREE.PerspectiveCamera(
       50,
       0.5 * aspect,
       1,
       1000
     )
-    camera.position.z = 2500
-    const cameraPerspective = new THREE.PerspectiveCamera(
-      50,
-      0.5 * aspect,
-      150,
-      1000
-    )
     scene.add(cameraPerspective)
+    // 透视相机边界可视化
     const cameraPerspectiveHelper = new THREE.CameraHelper(cameraPerspective)
     scene.add(cameraPerspectiveHelper)
+    cameraPerspective.rotation.y =  Math.PI
+
+
+    const cameraRig = new THREE.Group()
+    cameraRig.add(cameraPerspective)
+    scene.add(cameraRig)
+
+
+
+    const mesh = new THREE.Mesh(new THREE.SphereGeometry(
+      2, 16, 8
+    ),
+    new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      wireframe: true,
+    }))
+    scene.add(mesh)
+
+
+    const mesh2 = new THREE.Mesh(new THREE.SphereGeometry(
+      1, 16, 8
+    ),
+    new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      wireframe: true,
+    }))
+    mesh2.position.y = 2
+    mesh.add(mesh2)
+
+    const mesh3 = new THREE.Mesh(new THREE.SphereGeometry(
+      1, 16, 8
+    ),
+    new THREE.MeshBasicMaterial({
+      color: 0x0000ff,
+      wireframe: true,
+    }))
+    // mesh3 是透视相机的起始端
+    mesh3.position.z = 0
+    cameraRig.add(mesh3)
+
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true,
+    })
+    renderer.setSize(canvasWidth, canvasHeight)
+
+    renderer.autoClear = false // 这个需要设置，防止 多视角渲染时，会自动对场景清空
+
+    function render() {
+      const r = Date.now() * 0.0005
+      mesh.position.x = 5 * Math.cos(r)
+      mesh.position.z = 5 * Math.sin(r)
+      mesh.position.y = 5 * Math.sin(r)
+      // 设置月球的位置变化
+      mesh.children[0].position.x = 2 * Math.cos(2 * r)
+      mesh.children[0].position.z = 2 * Math.sin(r)
+      cameraPerspective.fov = 40 + (20 * Math.sin(2 * r))
+      cameraPerspective.far = mesh.position.length()
+      cameraPerspective.updateProjectionMatrix()
+      cameraPerspectiveHelper.update()
+      cameraPerspectiveHelper.visible = true
+
+
+      //
+      cameraRig.lookAt(mesh.position)
+      renderer.clear()
+
+
+      cameraPerspectiveHelper.visible = false
+      renderer.setViewport(
+        0, 0, canvasWidth / 2, canvasHeight
+      )
+      renderer.render(scene, cameraPerspective)
+      cameraPerspectiveHelper.visible = true
+
+      renderer.setViewport(
+        canvasWidth / 2, 0, canvasWidth / 2, canvasHeight
+      )
+      renderer.render(scene, camera)
+      requestAnimationFrame(render)
+    }
+
+
+    requestAnimationFrame(render)
+
+
+
 
   }
 
