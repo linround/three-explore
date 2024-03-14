a14b3b87097699741d5eda745443df6a873457f5
linyuan <yuanlincuc@gmail.com>
2024-03-14 15:38:44 +0800

    feat: texture change

--- src/component/imagesDialog.jsx
+++ src/component/imagesDialog.jsx
@@ -6,14 +6,32 @@ import leaf from '../assets/leaf.jpg'
 import perspective from '../assets/perspective.jpg'
 import sunset from '../assets/sunset.jpg'
 import PropTypes from 'prop-types'
-
+import { createRef, useState } from 'react'
+const images = [
+  cat, face, greyhound, leaf, perspective, sunset
+]
 
 export function ImagesDialog(props) {
-  const images = [
-    cat, face, greyhound, leaf, perspective, sunset
-  ]
+  const [imgs, setImgs] = useState(images)
+
   const { show, onClose, onSelect, } = props
 
+  const fileRef = createRef()
+
+  const onUploadClick = () => {
+    fileRef.current.click()
+  }
+  const onFileChange = (event) => {
+    const files = event.target.files
+    if (files.length === 0) return
+    const file = files[0]
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const src = e.target.result
+      setImgs([...imgs, src])
+    }
+    reader.readAsDataURL(file)
+  }
   return (
     <>
       <div
@@ -21,7 +39,7 @@ export function ImagesDialog(props) {
         style={{ display: show ? '' : 'none', }}>
         <div className={css.title}>选择下列的一张图片或上传图片：</div>
         <div className={css.imgContainer}>
-          {images.map((src, index) => (
+          {imgs.map((src, index) => (
             <img
               onClick={() => onSelect(src)}
               key={index}
@@ -32,7 +50,9 @@ export function ImagesDialog(props) {
         </div>
 
         <div>
+          <input type={'file'} ref={fileRef} onChange={onFileChange} hidden/>
           <button className={css.closeBtn} onClick={onClose}>关闭</button>
+          <button className={css.uploadBtn} onClick={onUploadClick}>上传图片</button>
         </div>
       </div>
     </>
