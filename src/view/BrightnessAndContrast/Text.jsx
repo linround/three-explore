3f8d6bf0b61009a8a04d8c10d8d6ab802988b1b2
linyuan <yuanlincuc@gmail.com>
2024-03-15 15:23:28 +0800

    feat: texture change

--- src/view/BrightnessAndContrast/Text.jsx
+++ src/view/BrightnessAndContrast/Text.jsx
@@ -1,8 +1,59 @@
+import { CodeText } from '../../component/codeText.jsx'
+
 export function Text() {
 
   return (
     <main>
       <h1>图片的明度/对比度</h1>
+      <section>
+        <p>调节图像的亮度和对比度（深浅部分之间的差异）。如果减弱对比度，则深浅部分之间的差异会减少并产生软和的图像。</p>
+        <p>反之，如果增强对比度，则深浅部分之间的差异会增加并产生锐利的图像。</p>
+        <h2>亮度调节</h2>
+        <CodeText>
+          <>
+            <p>
+                vec3 adjustBrightness(vec3 color, float brightness){'{'}
+            </p>
+            <p>
+                return color + vec3(brightness);
+            </p>
+            <p>
+              {'}'}
+            </p>
+          </>
+        </CodeText>
+        <h2>对比度调节</h2>
+        <CodeText>
+          <>
+            <p>
+              vec3 adjustContrast(vec3 color, float contrast){'{'}
+            </p>
+
+            <p>
+              if(contrast &gt; 0.){'{'}
+            </p>
+
+            <p>
+              color.rgb = (color.rgb-0.5)/(1.-contrast) + 0.5;
+            </p>
+
+            <p>
+              {'}'} else{'{'}
+            </p>
+
+            <p>
+              color.rgb = (color.rgb-0.5)/(1.+contrast) + 0.5;
+            </p>
+
+            <p>
+              {'}'}
+            </p>
+            <p>
+              return color;
+            </p>
+          </>
+        </CodeText>
+      </section>
     </main>
   )
 }
--- src/view/BrightnessAndContrast/fragmentShader.glsl
+++ src/view/BrightnessAndContrast/fragmentShader.glsl
@@ -15,6 +15,20 @@ mat2 roate2d(float angle) {
 }
 
 
+vec3 adjustBrightness(vec3 color, float brightness){
+    return color + vec3(brightness);
+}
+
+
+vec3 adjustContrast(vec3 color, float contrast){
+    if(contrast>0.){
+        color.rgb = (color.rgb-0.5)/(1.-contrast) + 0.5;
+    } else{
+        color.rgb = (color.rgb-0.5)/(1.+contrast) + 0.5;
+    }
+    return color;
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord ){
 
     // 这里计算的是当前顶点相对于视口所在的二维坐标点
@@ -30,13 +44,8 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
     // 最终对该坐标进行 纹理采样 获取到坐标点得颜色值
     vec4 color = texture2D(iChannel0, uv);
 
-    color.rgb += brightness;
-
-    if(contrast>0.){
-        color.rgb = (color.rgb-0.5)/(1.-contrast) + 0.5;
-    } else{
-        color.rgb = (color.rgb-0.5)/(1.+contrast) + 0.5;
-    }
+    color.rgb += adjustBrightness(color.rgb, brightness);
+    color.rgb = adjustContrast(color.rgb, contrast);
 
     fragColor=vec4(color.rgb,1.0);
 }
