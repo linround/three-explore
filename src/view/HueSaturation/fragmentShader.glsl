50afde47d7297b6f4206284269d35cd90cf3222a
linyuan <yuanlincuc@gmail.com>
2024-03-19 17:58:32 +0800

    feat:HueSaturation

--- src/view/HueSaturation/fragmentShader.glsl
+++ src/view/HueSaturation/fragmentShader.glsl
@@ -8,8 +8,27 @@ uniform float hue;
 
 
 
+vec4 adjustHue(vec4 color, float angle) {
+    float s = sin(angle), c = cos(angle);
+    vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c) + 1.0) / 3.0;
+    float len = length(color.rgb);
+    color.rgb = vec3(
+    dot(color.rgb, weights.xyz),
+    dot(color.rgb, weights.zxy),
+    dot(color.rgb, weights.yzx)
+    );
+    return color;
+}
 
-
+vec4 adjustSaturation(vec4 color, float saturation) {
+    float average = (color.r + color.g + color.b) / 3.0;
+    if (saturation > 0.0) {
+        color.rgb += (average - color.rgb) * (1.0 - 1.0 / (1.001 - saturation));
+    } else {
+        color.rgb += (average - color.rgb) * (-saturation);
+    }
+    return color;
+}
 
 void mainImage( out vec4 fragColor, in vec2 fragCoord ){
 
@@ -22,27 +41,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ){
     vec4 color = texture2D(iChannel0, uv);
 
     /**  hue adjustment  **/
-    float angle = hue * 3.14159265;
-    float s = sin(angle), c = cos(angle);
-
-
-    vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c) + 1.0) / 3.0;
-    float len = length(color.rgb);
-
-    color.rgb = vec3(
-    dot(color.rgb, weights.xyz),
-    dot(color.rgb, weights.zxy),
-    dot(color.rgb, weights.yzx)
-    );
+    color = adjustHue(color, hue);
 
 
     /* saturation adjustment */
-    float average = (color.r + color.g + color.b) / 3.0;
-    if (saturation > 0.0) {
-        color.rgb += (average - color.rgb) * (1.0 - 1.0 / (1.001 - saturation));
-    } else {
-        color.rgb += (average - color.rgb) * (-saturation);
-    }
+    color = adjustSaturation(color, saturation);
 
     fragColor= color;
 }
