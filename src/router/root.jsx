3dc3b3ed029a25a9db611dd7e7fb13b8752c149a
linyuan <yuanlincuc@gmail.com>
2024-03-29 10:54:48 +0800

    feat: add monitor

--- src/router/root.jsx
+++ src/router/root.jsx
@@ -1,8 +1,21 @@
 import { Outlet } from 'react-router-dom'
 import css from './root.module.less'
+import { addRecords, getRecords } from '@linround/commonapi'
+import { useEffect } from 'react'
 
 export default function Root() {
   const href = 'https://github.com/linround/three-explore'
+
+  useEffect(() => {
+    addRecords()
+      .then((response) => {
+        console.log('addRecords', response)
+        getRecords()
+          .then((response) => {
+            console.log('getRecords', response)
+          })
+      })
+  }, [])
   return (
     <div className={css.main}>
       <div className={css.head}>
