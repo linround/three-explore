3e11ec21cb0c1782c0c2ee13fa33df98b56e896c
linyuan <yuanlincuc@gmail.com>
2024-04-02 17:48:11 +0800

    feat: add monitor

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router.jsx'
-
+console.log('test')
 ReactDOM.createRoot(document.getElementById('root'))
   .render(<RouterProvider router={router} />)
